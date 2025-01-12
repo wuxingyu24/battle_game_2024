bd76354f23a116f206f19e213ddb9c4708ba41cd
wuxingyu24 <wuxingyu24@mails.tsinghua.edu.cn>
2025-01-12 22:54:34 +0800

    Update 59_tank.cpp

--- src/battle_game/core/units/59_tank.cpp
+++ src/battle_game/core/units/59_tank.cpp
@@ -1,4 +1,4 @@
-#include "tiny_tank.h"
+#include "59_tank.h"
 
 #include "battle_game/core/bullets/bullets.h"
 #include "battle_game/core/game_core.h"
@@ -11,7 +11,7 @@ uint32_t tank_body_model_index = 0xffffffffu;
 uint32_t tank_turret_model_index = 0xffffffffu;
 }  // namespace
 
-Tank::Tank(GameCore *game_core, uint32_t id, uint32_t player_id)
+wu_Tank::wu_Tank(GameCore *game_core, uint32_t id, uint32_t player_id)
     : Unit(game_core, id, player_id) {
   if (!~tank_body_model_index) {
     auto mgr = AssetsManager::GetInstance();
@@ -70,7 +70,7 @@ Tank::Tank(GameCore *game_core, uint32_t id, uint32_t player_id)
   }
 }
 
-void Tank::Render() {
+void wu_Tank::Render() {
   battle_game::SetTransformation(position_, rotation_);
   battle_game::SetTexture(0);
   battle_game::SetColor(game_core_->GetPlayerColor(player_id_));
@@ -79,13 +79,13 @@ void Tank::Render() {
   battle_game::DrawModel(tank_turret_model_index);
 }
 
-void Tank::Update() {
+void wu_Tank::Update() {
   TankMove(3.0f, glm::radians(180.0f));
   TurretRotate();
   Fire();
 }
 
-void Tank::TankMove(float move_speed, float rotate_angular_speed) {
+void wu_Tank::TankMove(float move_speed, float rotate_angular_speed) {
   auto player = game_core_->GetPlayer(player_id_);
   if (player) {
     auto &input_data = player->GetInputData();
@@ -117,7 +117,7 @@ void Tank::TankMove(float move_speed, float rotate_angular_speed) {
   }
 }
 
-void Tank::TurretRotate() {
+void wu_Tank::TurretRotate() {
   auto player = game_core_->GetPlayer(player_id_);
   if (player) {
     auto &input_data = player->GetInputData();
@@ -130,7 +130,7 @@ void Tank::TurretRotate() {
   }
 }
 
-void Tank::Fire() {
+void wu_Tank::Fire() {
   if (fire_count_down_ == 0) {
     auto player = game_core_->GetPlayer(player_id_);
     if (player) {
@@ -149,18 +149,18 @@ void Tank::Fire() {
   }
 }
 
-bool Tank::IsHit(glm::vec2 position) const {
+bool wu_Tank::IsHit(glm::vec2 position) const {
   position = WorldToLocal(position);
   return position.x > -0.8f && position.x < 0.8f && position.y > -1.0f &&
          position.y < 1.0f && position.x + position.y < 1.6f &&
          position.y - position.x < 1.6f;
 }
 
-const char *Tank::UnitName() const {
+const char *wu_Tank::UnitName() const {
   return "Tiny Tank";
 }
 
-const char *Tank::Author() const {
+const char *wu_Tank::Author() const {
   return "LazyJazz";
 }
 } 
